85f459a8a0cc42467f94f8998c16b9cda125dbb0
Niki Howe <nikihowe@gmail.com>
2022-05-25 23:02:50 +0100

    Deanonymize author (for now)

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-# (c) 2022 Anonymous Author
+# (c) 2022 Nikolaus Howe
 from experiments.cleaning_robot_experiments import run_cleaning_robot_experiment
 from experiments.two_state_mdp_experiments import run_two_state_mdp_experiment
 
